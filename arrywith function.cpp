8e1ed49f0499c6066be23358379a25864f3a1cc1
Prince kumar Singh <122429010+singhprince9875@users.noreply.github.com>
2023-03-24 00:33:18 +0530

    Add files via upload

---
+++ arrywith function.cpp
@@ -0,0 +1,33 @@
+/*#include<iostream>
+#include<array>
+using namespace std;
+void printArray(int arr[],int size){
+cout<<"printing number is"<<endl; 
+   for(int i= 0;i<size;i++)
+   cout<<arr[i] << " ";}
+
+int main(){
+   int firstArray[10]={1,2};
+    printArray(firstArray,10);
+     //
+    int second[12]={2,3,4,5};
+    printArray(second,12);
+        
+    }*/
+//
+#include<iostream>
+using namespace std;
+int main(){int n,p;
+    cout<<"enter the size";
+    cin>>n;
+    cout<<"enter the elements";
+    for(int i=0;i<n;i++){
+    cin>>p;}
+    cout<<p<<endl;
+    
+    
+    //cout<<"your elements are"<<endl;
+    //for(int j=0;j<n;j++){
+      //  cout<<p<<endl;
+    }
+
